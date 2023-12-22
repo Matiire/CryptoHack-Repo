76f613184aca67c8ccc85b39619f32290519605a
Matiire <114078222+Matiire@users.noreply.github.com>
2023-12-22 22:02:17 +0000

    .

---
+++ RSA/RSA Starter 4/RSA Starter 4.py
@@ -0,0 +1,10 @@
+from Crypto.Util.number import inverse
+
+p = 857504083339712752489993810777 
+q = 1029224947942998075080348647219 
+e = 65537
+
+totient = (p - 1)*(q - 1) 
+d = inverse(e, totient) 
+
+print(d)
